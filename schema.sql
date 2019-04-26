c6651f15bf7505f88537f7a1aeb00fbeb383dd78
Anand Chowdhary <mail@anandchowdhary.com>
2019-04-26 10:19:17 +0530

    :wrench: Update schema for events

--- schema.sql
+++ schema.sql
@@ -39,9 +39,11 @@ CREATE TABLE `events` (
   `userId` int(11) DEFAULT NULL,
   `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `data` text,
+  `ipAddress` varchar(255) DEFAULT NULL,
+  `userAgent` text,
   `createdAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Table structure for memberships
