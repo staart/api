16080bd32a691547d520d3dceb1211c4db996bb2
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-14 00:06:22 +0200

    :lock: Increase API keys size from 10 to 20

--- src/crud/user.ts
+++ src/crud/user.ts
@@ -214,8 +214,8 @@ export const getApiKey = async (apiKey: string) => {
  * Create an API key
  */
 export const createApiKey = async (apiKey: ApiKey) => {
-  apiKey.apiKey = cryptoRandomString({ length: 10, type: "url-safe" });
-  apiKey.secretKey = cryptoRandomString({ length: 10, type: "url-safe" });
+  apiKey.apiKey = cryptoRandomString({ length: 20, type: "url-safe" });
+  apiKey.secretKey = cryptoRandomString({ length: 20, type: "url-safe" });
   apiKey.createdAt = new Date();
   apiKey.updatedAt = apiKey.createdAt;
   deleteItemFromCache(CacheCategories.API_KEYS, apiKey.userId);
