54bc37f21c1f9bb175a6e5d81fd581cf3a8ed334
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-26 13:25:29 +0200

    :sparkles: Complete user controller

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -19,112 +19,122 @@ import {
   Post,
   Put,
   Delete,
-  Middleware,
-  Controller
+  Controller,
+  ClassMiddleware
 } from "@overnightjs/core";
-
-const routeUserId = async (req: Request, res: Response) => {
-  let id = req.body.id || req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getUserFromId(id, res.locals.token.id));
-};
-
-const routeUserUpdate = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  await updateUserForUser(res.locals.token.id, id, req.body, res.locals);
-  res.json({ success: true });
-};
-
-const routeUserRecentEvents = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getRecentEventsForUser(res.locals.token.id, id));
-};
-const routeUserMemberships = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getMembershipsForUser(res.locals.token.id, id));
-};
-
-const routeUserAllData = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getAllDataForUser(res.locals.token.id, id));
-};
-
-const routeUserDelete = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await deleteUserForUser(res.locals.token.id, id, res.locals));
-};
-
-const routeUserApiKeysGet = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getApiKeysForUser(res.locals.token.id, id));
-};
-
-const routeUserApiKeysPut = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await createApiKeyForUser(res.locals.token.id, id, res.locals));
-};
-
-const routeUserApiKeyGet = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  const apiKey = req.params.apiKey;
-  if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getApiKeyForUser(res.locals.token.id, id, apiKey));
-};
-
-const routeUserApiKeyUpdate = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  const apiKey = req.params.apiKey;
-  if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(
-    await updateApiKeyForUser(
-      res.locals.token.id,
-      id,
-      apiKey,
-      req.body,
-      res.locals
-    )
-  );
-};
-
-const routeUserApiKeyDelete = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  const apiKey = req.params.apiKey;
-  if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(
-    await deleteApiKeyForUser(res.locals.token.id, id, apiKey, res.locals)
-  );
-};
-
-const routeUserNotificationsGet = async (req: Request, res: Response) => {
-  let id = req.params.id;
-  if (id === "me") id = res.locals.token.id;
-  if (!id) throw new Error(ErrorCode.MISSING_FIELD);
-  res.json(await getNotificationsForUser(res.locals.token.id, id));
-};
+import { authHandler } from "../helpers/middleware";
 
 @Controller("user")
+@ClassMiddleware(authHandler)
 export class UserController {
   @Get(":id")
-  get(req: Request, res: Response) {
-    console.log(req.params.id);
-    res.status(200).json({ msg: "get_called" });
+  async get(req: Request, res: Response) {
+    let id = req.body.id || req.params.id;
+    console.log(id, "Locals are", res.locals);
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getUserFromId(id, res.locals.token.id));
+  }
+
+  @Post(":id")
+  async patch(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    await updateUserForUser(res.locals.token.id, id, req.body, res.locals);
+    res.json({ success: true });
+  }
+
+  @Delete(":id")
+  async delete(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await deleteUserForUser(res.locals.token.id, id, res.locals));
+  }
+
+  @Get(":id/recent-events")
+  async getRecentEvents(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getRecentEventsForUser(res.locals.token.id, id));
+  }
+
+  @Get(":id/memberships")
+  async getMemberships(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getMembershipsForUser(res.locals.token.id, id));
+  }
+
+  @Get(":id/data")
+  async getUserData(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getAllDataForUser(res.locals.token.id, id));
+  }
+
+  @Get(":id/api-keys")
+  async getUserApiKeys(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getApiKeysForUser(res.locals.token.id, id));
+  }
+
+  @Put(":id/api-keys")
+  async putUserApiKeys(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await createApiKeyForUser(res.locals.token.id, id, res.locals));
+  }
+
+  @Get(":id/api-keys/:apiKey")
+  async getUserApiKey(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    const apiKey = req.params.apiKey;
+    if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getApiKeyForUser(res.locals.token.id, id, apiKey));
+  }
+
+  @Post(":id/api-keys/:apiKey")
+  async patchUserApiKey(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    const apiKey = req.params.apiKey;
+    if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(
+      await updateApiKeyForUser(
+        res.locals.token.id,
+        id,
+        apiKey,
+        req.body,
+        res.locals
+      )
+    );
+  }
+
+  @Delete(":id/api-keys/:apiKey")
+  async deleteUserApiKey(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    const apiKey = req.params.apiKey;
+    if (!id || !apiKey) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(
+      await deleteApiKeyForUser(res.locals.token.id, id, apiKey, res.locals)
+    );
+  }
+
+  @Get(":id/notifications")
+  async getUserNotifications(req: Request, res: Response) {
+    let id = req.params.id;
+    if (id === "me") id = res.locals.token.id;
+    if (!id) throw new Error(ErrorCode.MISSING_FIELD);
+    res.json(await getNotificationsForUser(res.locals.token.id, id));
   }
 }
