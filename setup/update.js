eadc51f8a894369ffa6a506b2fd3576b01732fdf
Anand Chowdhary <mail@anandchowdhary.com>
2019-07-02 15:34:22 +0200

    :bug: Use Yarn for staart-manager

--- setup/update.js
+++ setup/update.js
@@ -1,8 +1,10 @@
+const dotenv = require("dotenv");
 const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
 const shell = require("shelljs");
 
+dotenv.config();
 const packageUrl =
   "https://raw.githubusercontent.com/o15y/staart/master/package.json";
 
@@ -20,7 +22,11 @@ const checkUpdate = async () => {
     (await fs.readFile(path.join(__dirname, "..", "package.json"))).toString()
   );
   if (i.name !== "staart-manager") {
-    shell.exec("npm install --save-dev staart-manager");
+    if (process.env.USE_NPM) {
+      shell.exec("npm install --save-dev staart-manager");
+    } else {
+      shell.exec("yarn add -D staart-manager");
+    }
   }
   return;
 };
