fc4a769c8da8b3f33d1c62b4eb265b66ab9922ae
Anand Chowdhary <mail@anandchowdhary.com>
2019-08-31 20:59:33 +0530

    :sparkles: Add basic update script

---
+++ setup/check-update.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv");
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const shell = require("shelljs");
+const hasYarn = require("has-yarn");
+const yourPkg = require("../package.json");
+
+dotenv.config();
+const packageUrl =
+  "https://raw.githubusercontent.com/o15y/staart/master/package.json";
+
+const checkUpdate = async () => {
+  const pkg = (await axios.get(packageUrl)).data;
+  const v = yourPkg["staart-version"];
+  console.log("Most recent version is", pkg.version);
+  console.log("Your version is", v);
+  if (v !== pkg.version) {
+    console.log("🚨  Staart update required");
+  }
+  const i = JSON.parse(
+    (await fs.readFile(path.join(__dirname, "..", "package.json"))).toString()
+  );
+  if (
+    i.name !== "staart-manager" &&
+    !Object.keys(i.devDependencies).includes("staart-manager")
+  ) {
+    if (process.env.USE_NPM || !hasYarn()) {
+      shell.exec("npm install --save-dev staart-manager");
+    } else {
+      shell.exec("yarn add -D staart-manager");
+    }
+  }
+  return;
+};
+
+checkUpdate()
+  .then(() => {})
+  .catch(error => console.log("ERROR", error));
--- setup/update.js
+++ setup/update.js
@@ -1,39 +1,11 @@
-const dotenv = require("dotenv");
-const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
-const shell = require("shelljs");
-const hasYarn = require("has-yarn");
-const yourPkg = require("../package.json");
+var shell = require("shelljs");
 
-dotenv.config();
-const packageUrl =
-  "https://raw.githubusercontent.com/o15y/staart/master/package.json";
+if (!shell.which("git")) {
+  shell.echo("Sorry, this script requires git");
+  shell.exit(1);
+}
 
-const checkUpdate = async () => {
-  const pkg = (await axios.get(packageUrl)).data;
-  const v = yourPkg["staart-version"];
-  console.log("Most recent version is", pkg.version);
-  console.log("Your version is", v);
-  if (v !== pkg.version) {
-    console.log("🚨  Staart update required");
-  }
-  const i = JSON.parse(
-    (await fs.readFile(path.join(__dirname, "..", "package.json"))).toString()
-  );
-  if (
-    i.name !== "staart-manager" &&
-    !Object.keys(i.devDependencies).includes("staart-manager")
-  ) {
-    if (process.env.USE_NPM || !hasYarn()) {
-      shell.exec("npm install --save-dev staart-manager");
-    } else {
-      shell.exec("yarn add -D staart-manager");
-    }
-  }
-  return;
-};
-
-checkUpdate()
-  .then(() => {})
-  .catch(error => console.log("ERROR", error));
+if (shell.exec("git pull git@github.com:o15y/staart").code !== 0) {
+  shell.echo("Error: Git commit failed");
+  shell.exit(1);
+}
