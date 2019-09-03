16cdab0cf9d8e118a0c22c900202cb1e28b1c3f6
Anand Chowdhary <mail@anandchowdhary.com>
2019-09-03 18:27:29 +0530

    :sparkles: Delete logs older than 3m (fixed #284)

--- src/crons/daily.ts
+++ src/crons/daily.ts
@@ -1,12 +1,36 @@
 import { CronJob } from "cron";
+import { elasticSearch } from "../helpers/elasticsearch";
+import { ELASTIC_LOGS_PREFIX } from "../config";
+import ms from "ms";
 
 export default () => {
   new CronJob(
     "0 0 * * *",
-    () => {
-      console.log("Once per day", new Date());
+    async () => {
+      await deleteOldLogs();
     },
     undefined,
     true
   );
+};
+
+const deleteOldLogs = async () => {
+  return await elasticSearch.deleteByQuery({
+    index: `${ELASTIC_LOGS_PREFIX}*`,
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              range: {
+                date: {
+                  lte: new Date(new Date().getTime() - ms("92 days"))
+                }
+              }
+            }
+          ]
+        }
+      }
+    }
+  });
 };
