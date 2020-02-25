0e5b3218d0cb536ca9d4018204c9ea7d6f725a5b
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-25 21:56:06 +0100

    :heavy_plus_sign: Add @staart/server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import "@babel/polyfill";
-import helmet from "helmet";
-import cors from "cors";
-import responseTime from "response-time";
-import { json, urlencoded } from "body-parser";
 import { Server } from "@overnightjs/core";
 import { success } from "@staart/errors";
+import { setupMiddleware } from "@staart/server";
 
 // This `join` is required for static files in app.ts
 import { join } from "path";
@@ -16,22 +13,17 @@ import {
   speedLimitHandler
 } from "./helpers/middleware";
 import { Request, Response } from "express";
-import { DISALLOW_OPEN_CORS } from "./config";
 
 export class Staart extends Server {
   constructor() {
     super();
     this.setupHandlers();
     this.setupControllers();
-    if (!DISALLOW_OPEN_CORS) this.app.use(errorHandler);
+    this.app.use(errorHandler);
   }
 
   private setupHandlers() {
-    this.app.use(cors({ maxAge: 600 }));
-    this.app.use(helmet({ hsts: { maxAge: 31536000, preload: true } }));
-    this.app.use(json({ limit: "50mb" }));
-    this.app.use(urlencoded({ extended: true }));
-    this.app.use(responseTime());
+    setupMiddleware(this.app);
     this.app.use(trackingHandler);
     this.app.use(rateLimitHandler);
     this.app.use(speedLimitHandler);
