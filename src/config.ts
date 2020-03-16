fab16697281c89493d961eb4afe604e910600003
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-16 23:50:32 +0530

    :bug: Strict check bool envs in config (#1046)

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
 config();
 
+export const bool = (val?: string | boolean) =>
+  String(val).toLowerCase() === "true";
+
 // Server
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 80;
 export const BASE_URL = process.env.BASE_URL || "";
@@ -62,7 +65,7 @@ export const CACHE_CHECK_PERIOD = process.env.CACHE_CHECK_PERIOD
 
 // Email
 export const FRONTEND_URL = process.env.FRONTEND_URL || "https://example.com";
-export const ALLOW_DISPOSABLE_EMAILS = !!process.env.DISPOSABLE_EMAIL;
+export const ALLOW_DISPOSABLE_EMAILS = bool(process.env.DISPOSABLE_EMAIL);
 export const TEST_EMAIL = process.env.TEST_EMAIL || "staart@mailinator.com";
 /// If you want to use AWS SES to send emails:
 export const SES_EMAIL = process.env.SES_EMAIL || "";
@@ -92,7 +95,7 @@ export const TOKEN_EXPIRY_REFRESH = process.env.TOKEN_EXPIRY_REFRESH || "30d";
 export const TOKEN_EXPIRY_API_KEY_MAX = process.env.TOKEN_EXPIRY_API_KEY_MAX
   ? parseInt(process.env.TOKEN_EXPIRY_API_KEY_MAX)
   : 10413685800000; // 2299-12-31 is the default maximum expiry (also what Microsoft uses)
-export const DISALLOW_OPEN_CORS = !!process.env.DISALLOW_OPEN_CORS;
+export const DISALLOW_OPEN_CORS = bool(process.env.DISALLOW_OPEN_CORS);
 
 // OAuth2 credentials
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
