dc79805b16a74bb6fa8a942bb5fcc1a355df537b
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-22 21:08:12 +0530

    :heavy_plus_sign: Add nodemailer

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EmailConfig } from './email.interface';
+import nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService {
