1da5f24187028f597355347ac890105cc6ad6735
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-22 21:50:05 +0530

    :construction: Use memoized fs reader

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -5,12 +5,16 @@ import PQueue from 'p-queue';
 import pRetry from 'p-retry';
 import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import mem from 'mem';
 
 @Injectable()
 export class EmailService {
   transport: Mail;
   config: EmailConfig;
   queue = new PQueue({ concurrency: 1 });
+  readTemplate = mem(this.readTemplateUnmemoized);
 
   constructor(private configService: ConfigService) {
     this.config = this.configService.get<EmailConfig>('email');
@@ -38,7 +42,14 @@ export class EmailService {
     );
   }
 
-  private async sendEmail(options: Mail.Options) {
+  private async sendEmail(options: Mail.Options & EmailOptions) {
+    if (options.template) {
+      const template = await this.readTemplate(options.template);
+    }
     return this.transport.sendMail(options);
+  }
+
+  private async readTemplateUnmemoized(name: string) {
+    return fs.readFile(join('.', 'src', 'templates', `${name}.md`), 'utf8');
   }
 }
