b3ec3fc486faddb15cf1f0a6af2c67050da2b0dc
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-27 20:00:18 +0530

    :sparkles: Add helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
