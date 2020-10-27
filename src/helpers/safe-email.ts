75fc733ff3b58aa3f922cf451120be507db401fa
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-27 19:28:25 +0530

    :recycle: Lowercase, remove plus from email

--- src/helpers/safe-email.ts
+++ src/helpers/safe-email.ts
@@ -1,1 +1,8 @@
-export const safeEmail = (email: string) => email;
+/**
+ * Converts an email address to a unqiue, safe email
+ * @param email - Valid email address
+ */
+export const safeEmail = (input: string) => {
+  const [user, domain] = input.split('@');
+  return `${user.split('+')[0]}@${domain}`.toLowerCase();
+};
