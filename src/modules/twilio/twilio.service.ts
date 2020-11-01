ec9ec37b7bf011f76ce08198a8ec515d4846c140
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-01 14:46:49 +0530

    :sparkles: Add Twilio module

--- src/modules/twilio/twilio.service.ts
+++ src/modules/twilio/twilio.service.ts
@@ -1,17 +1,47 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import Twilio from 'twilio';
+import twilio from 'twilio';
+import PQueue from 'p-queue';
+import pRetry from 'p-retry';
+import TwilioClient from 'twilio/lib/rest/Twilio';
+import { MessageListInstanceCreateOptions } from 'twilio/lib/rest/api/v2010/account/message';
 
 @Injectable()
 export class TwilioService {
-  twilio: Twilio;
+  twilio: TwilioClient;
   logger = new Logger('twilio');
+  private queue = new PQueue({ concurrency: 1 });
 
   constructor(private configService: ConfigService) {
-    const twilioApiKey = this.configService.get<string>('sms.twilioApiKey');
-    if (!twilioApiKey) this.logger.warn('Twilio API key not found');
-    this.twilio = new Twilio(twilioApiKey ?? '', {
-      apiVersion: '2020-08-27',
-    });
+    const twilioAccountSid = this.configService.get<string>(
+      'sms.twilioAccountSid',
+    );
+    const twilioAuthToken = this.configService.get<string>(
+      'sms.twilioAuthToken',
+    );
+    if (!twilioAccountSid || !twilioAuthToken)
+      this.logger.warn('Twilio account SID/auth token not found');
+    this.twilio = twilio(twilioAccountSid ?? '', twilioAuthToken ?? '');
+  }
+
+  send(options: MessageListInstanceCreateOptions) {
+    this.queue
+      .add(() =>
+        pRetry(() => this.sendSms(options), {
+          retries: 3,
+          onFailedAttempt: (error) => {
+            this.logger.error(
+              `SMS to ${options.to} failed, retrying (${error.retriesLeft} attempts left)`,
+              error.name,
+            );
+          },
+        }),
+      )
+      .then(() => {})
+      .catch(() => {});
+  }
+
+  private async sendSms(options: MessageListInstanceCreateOptions) {
+    return this.twilio.messages.create(options);
   }
 }
