c401a33087e93891a9ba92512d8a8e7dbf4da632
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-02 17:26:25 +0530

    :recycle: Normalize +, . in emails

--- src/helpers/safe-email.ts
+++ src/helpers/safe-email.ts
@@ -1,8 +1,9 @@
+import normalize from 'normalize-email';
+
 /**
  * Converts an email address to a unqiue, safe email
  * @param email - Valid email address
  */
 export const safeEmail = (input: string) => {
-  const [user, domain] = input.split('@');
-  return `${user.split('+')[0]}@${domain}`.toLowerCase();
+  return normalize(input.toLowerCase().trim());
 };
