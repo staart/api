fb46eeb4fa73062c77ada03772ce3438986e35c3
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-03 15:16:24 +0530

    :bug: Import StripeModule

--- src/modules/api-keys/api-keys.module.ts
+++ src/modules/api-keys/api-keys.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StripeModule } from '../stripe/stripe.module';
 import { TokensModule } from '../tokens/tokens.module';
 import { ApiKeyController } from './api-keys.controller';
 import { ApiKeysService } from './api-keys.service';
 
 @Module({
-  imports: [PrismaModule, TokensModule],
+  imports: [PrismaModule, TokensModule, StripeModule],
   controllers: [ApiKeyController],
   providers: [ApiKeysService],
 })
