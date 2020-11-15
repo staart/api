95678056322469c9a04124a4f772614404cbb216
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-15 16:50:34 +0530

    :bug: Validate new email

--- src/modules/emails/emails.dto.ts
+++ src/modules/emails/emails.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 
 export class CreateEmailDto {
-  @IsString()
+  @IsEmail()
   @IsNotEmpty()
   email!: string;
 }
