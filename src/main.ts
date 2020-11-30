eb98a1fbd9d59bcaeb499cb9268c3ec99da86ad4
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-30 22:32:04 +0530

    :recycle: Listen on env PORT

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
   app.use(responseTime());
   app.useStaticAssets(join(__dirname, '..', 'static'));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
